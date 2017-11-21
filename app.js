f4f1cc33944e6bbc8c607ccf2c0447753884469a
Kimsejin97 <sjk97@naver.com>
2017-11-21 18:11:28 +0900

    2

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var passport = require('passport');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var questions = require('./routes/questions');
+var events = require('./routes/events');
 
 var passportConfig = require('./lib/passport-config');
 
@@ -89,7 +89,7 @@ app.use(function(req, res, next) {
 // Route
 app.use('/', index);
 app.use('/users', users);
-app.use('/questions', questions);
+app.use('/events', events);
 require('./routes/auth')(app, passport);
 
 // catch 404 and forward to error handler
