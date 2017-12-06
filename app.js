d764619a7b7125de4260b6edf9e44b0b71c99ea6
Kimsejin97 <sjk97@naver.com>
2017-12-06 23:41:53 +0900

    9번 heroku

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ app.locals.querystring = require('querystring');
 //=======================================================
 mongoose.Promise = global.Promise; // ES6 Native Promise를 mongoose에서 사용한다.
 const connStr = 'mongodb://localhost/mjdb2';
-// 아래는 mLab을 사용하는 경우의 예: 본인의 접속 String으로 바꾸세요.
-// const connStr = 'mongodb://dbuser1:mju12345@ds113825.mlab.com:13825/sampledb1';
+아래는 mLab을 사용하는 경우의 예: 본인의 접속 String으로 바꾸세요.
+const connStr = 'mongodb://dbuser1:dbuser1@ds259255.mlab.com:59255/project_database';
 mongoose.connect(connStr, {useMongoClient: true });
 mongoose.connection.on('error', console.error);
 
